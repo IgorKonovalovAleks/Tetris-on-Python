e1b6d4555e6c04871be7a89080fb73978a7cd685
IgorKonovalovAleks <igkonov.yandex.ru>
2018-10-24 20:07:58 +0300

    Made: Moving pieces per 0.5 seconds, making new pieces Next: Key Press events

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import random
-import random
 import sys
 
 from PyQt5.QtCore import QBasicTimer, Qt
@@ -28,19 +27,19 @@ class Board(QFrame):
         self.board = [0 for i in range(200)]
         self.timer = QBasicTimer()
         self.setFocusPolicy(Qt.StrongFocus)
-        self.newTurn()
+        self.figure = Figure()
+        self.timer.start(500, self)
 
     def newTurn(self):
         self.dropFigure()
         self.figure = Figure()
 
     def dropFigure(self):
+        color = DATA.getColors().index(self.figure.color)
         for i in range(4):
             coord = self.figure.position[i][0] + self.figure.position[i][1] * 10
-            for j in range(7):
-                checkColor = DATA.getColor(j)
-                if checkColor == self.figure.color:
-                    self.board[coord] = j
+            self.board[coord] = color
+        print(self.board)
 
     def paintEvent(self, e):
         qp = QPainter()
@@ -48,15 +47,23 @@ class Board(QFrame):
         qp.setPen(QColor(200, 100, 100))
         for i in range(200):
             x = i % 10
-            y = i - x * 10
+            y = (i - x) / 10
             if self.board[i] != 0:
                 tpl = DATA.getColor(self.board[i])
                 qp.setBrush(QColor(tpl[0], tpl[1], tpl[2]))
                 qp.drawRect(x * self.cube, y * self.cube, self.cube, self.cube)
         qp.setBrush(QColor(self.figure.color[0], self.figure.color[1], self.figure.color[2]))
         for i in range(4):
-            qp.drawRect(self.figure.position[i][0] * self.cube, self.figure.position[i][1] * self.cube, self.cube, self.cube)
+            qp.drawRect(self.figure.position[i][0] * self.cube,
+                        self.figure.position[i][1] * self.cube, self.cube, self.cube)
         qp.end()
+
+    def timerEvent(self, event):
+        if self.figure.move():
+            pass
+        else:
+            self.newTurn()
+        self.update()
 
 
 class DATA(object):
@@ -66,11 +73,16 @@ class DATA(object):
         return DATA._tetrominoe[fig]
 
     @staticmethod
+    def getColors():
+        return DATA._colors
+
+    @staticmethod
     def getColor(fig):
         return DATA._colors[fig]
 
-    _colors = ((198, 3, 3), (198, 123, 2), (109, 198, 1), (1, 198, 102), (1, 171, 198), (27, 1, 198), (168, 1, 198))
-    _tetrominoe = (((0, 0), (0, 1), (1, 1), (1, 2)),
+    _colors = ((255, 255, 255), (198, 3, 3), (198, 123, 2), (109, 198, 1), (1, 198, 102), (1, 171, 198), (27, 1, 198), (168, 1, 198))
+    _tetrominoe = ((),
+                   ((0, 0), (0, 1), (1, 1), (1, 2)),
                    ((1, 0), (1, 1), (0, 1), (0, 2)),
                    ((0, 0), (1, 0), (1, 1), (1, 2)),
                    ((0, 0), (0, 1), (0, 2), (0, 3)),
@@ -86,7 +98,10 @@ class Figure(object):
     color = (0, 0, 0)
 
     def __init__(self):
-        num = random.randint(0, 6)
+        self.shape = []
+        self.position = []
+        self.color = None
+        num = random.randint(1, 7)
         self._make(num)
 
     def _make(self, fig):
@@ -98,7 +113,12 @@ class Figure(object):
         self.position = self.shape.copy()
 
     def move(self):
-        pass
+        for i in range(4):
+            if self.position[i][1] == 19:
+                return False
+        for i in range(4):
+            self.position[i][1] += 1
+        return True
 
 
 if __name__ == '__main__':
